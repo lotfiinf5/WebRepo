a0ff183e8f7f34df0def2c7d4642dc39505e54d5
User <User@DESKTOP-E2R42SK>
2019-03-24 19:36:16 -0400

    add modif

--- testjenkins/src/com/cont/Aaaaaaaa.java
+++ testjenkins/src/com/cont/Aaaaaaaa.java
@@ -3,9 +3,11 @@ package com.cont;
 public class Aaaaaaaa {
 
 	public static void main(String[] args) {
-		for (int i = 0; i <16; i++) {
+		System.out.println("---------------------------->");
+		for (int i = 0; i <11; i++) {
 			System.out.println("---------------->  "+i);
 		}
+		System.out.println("---------------------------->");
 	}
 
 }
