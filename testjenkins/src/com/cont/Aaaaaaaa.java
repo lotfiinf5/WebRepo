82a493fc52a32308b1a7bd49a483c8d23fe81a0b
lotfi <dinodidine213@gmail.com>
2021-08-09 08:32:45 -0400

    add aa to outptut

--- testjenkins/src/com/cont/Aaaaaaaa.java
+++ testjenkins/src/com/cont/Aaaaaaaa.java
@@ -5,7 +5,7 @@ public class Aaaaaaaa {
 	public static void main(String[] args) {
 		System.out.println("---------------------------->");
 		for (int i = 0; i <11; i++) {
-			System.out.println("---------------->  "+i);
+			System.out.println("---------------->  oo"+i);
 		}
 		System.out.println("---------------------------->");
 		
